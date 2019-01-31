de768e3e0c42b9d2dabda0c908b2029ba1de3c44
Piotr <piotr@MacBook-Pro-Piotr.local>
2019-01-31 01:15:54 +0100

    summernote fix with scrolling

--- assets/inject_summernote_to_admin.js
+++ assets/inject_summernote_to_admin.js
@@ -1,5 +1,6 @@
 // inject summernote css into the admin interface
-$('head').append('<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-lite.css" rel="stylesheet">')
+$('head').append('<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-lite.css" rel="stylesheet">');
+$('head').append('<style>.summernote { margin: 10px 0; }</style>');
 
 // * ———————————————————————————————————————————————————————— * //
 // *	summernote directive
@@ -7,15 +8,15 @@ $('head').append('<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/
 enduro_admin_app.compileProvider
 	.directive('summernote', function () {
 		return {
+      restrict: 'E',
 			link: function (scope, element, attr) {
-        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-lite.js', function () {
+        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-lite.js', function () {
 
           $(element[0]).summernote(brick_admin_settings.enduro_summernote);
-          
+
           $(element[0]).on('summernote.change', function(_, contents) {
             scope.context[scope.terminatedkey] = contents;
           });
-          
 
 					scope.$watch('current_culture', function () {
             $(element[0]).summernote('code', scope.context[scope.terminatedkey] || '');
